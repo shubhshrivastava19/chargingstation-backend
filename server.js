1461b04075e1ca38b4068c0e9d1dbdcfe2dbce6b
shubhshrivastava19 <shubhshrivastav39@gmail.com>
2025-07-28 14:28:56 +0530

    fix1

--- server.js
+++ server.js
@@ -10,12 +10,11 @@ connectDB();
 
 const app = express();
 app.use(cors());
-app.options('*', cors());
 
 app.use(express.json());
 
 app.use('/api/auth', Auth );
-app.use('/api/stations', Stations);
+app.use('`/api/stations`', Stations);
 
 const PORT = process.env.PORT || 3009;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
